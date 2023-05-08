a99e77e5e6e1078d85c5e8a69cb99d64b49079ee
fernando.pinho <fernando.pinho@petrobras.com.br>
2023-05-08 12:03:07 -0300

    Aulas 11 e 12: Criando estilo na seleção dos itens e dividindo o componente de detalhe para ser reaproveitado.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.model';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit{
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero?: Hero;
 
-  hero: Hero = {
-    id: 1,
-    name: 'Homem Aranha'
-
-  };
-
-  ngOnInit(): void {
-
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
   }
 
 }
